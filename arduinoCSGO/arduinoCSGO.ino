bd866a57f3ce5bb7cd9d4fce5a2f93bca82c1fe8
osc3b <osc3boronat@gmail.com>
2019-03-30 16:43:58 +0100

    Funciona algo! Primera subida del codigo a usar en la Arduino.
    Todo muy verde (ja,ja). Falta conectar el java al CSGO.
    
    Al iniciar el programa conecta, hace el flasheo inicial del programa,
    y manda un '1' para poner todas las luces en verde.

---
+++ arduinoCSGO/arduinoCSGO.ino
@@ -0,0 +1,39 @@
+#include "FastLED.h"
+
+#define NUM_LEDS 110
+#define DATA_PIN 6
+
+// Baudrate, higher rate allows faster refresh rate and more LEDs (defined in /etc/boblight.conf)
+#define serialRate 1000000
+
+// Initialise LED-array
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  // Use NEOPIXEL to keep true colors
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  
+  // Initial RGB flash
+  LEDS.showColor(CRGB(255, 0, 0));
+  delay(500);
+  LEDS.showColor(CRGB(0, 255, 0));
+  delay(500);
+  LEDS.showColor(CRGB(0, 0, 255));
+  delay(500);
+  LEDS.showColor(CRGB(0, 0, 0));
+  
+  Serial.begin(serialRate); //Comenzar la comunicación
+}
+
+void loop() {  
+  memset(leds, 0, NUM_LEDS * sizeof(struct CRGB));
+  // Read the transmission data and set LED values
+  while(!Serial.available());
+  if(Serial.read() == '1'){
+    for (uint8_t i = 0; i < NUM_LEDS; i++) {
+      leds[i].setRGB(0,255,0); //Pone todos los leds a verde
+    }
+    // Shows new values
+    FastLED.show();
+  }
+}
--- src/main/java/me/osc3b/csgoleds/CSGOLeds.java
+++ src/main/java/me/osc3b/csgoleds/CSGOLeds.java
@@ -18,6 +18,11 @@ public class CSGOLeds {
         conectarArduino();
         Logger.getLogger(CSGOLeds.class.getName()).log(Level.INFO, null, "Arduino conectada correctamente");
         try {
+            Thread.sleep(2500);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(CSGOLeds.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
             arduino.sendData("1"); //Envia inicialización al Arduino
         } catch (ArduinoException | SerialPortException ex) {
             Logger.getLogger(CSGOLeds.class.getName()).log(Level.SEVERE, null, ex);
@@ -26,17 +31,17 @@ public class CSGOLeds {
     
     public void conectarArduino(){
         try {
-            arduino.arduinoTX("COM3", 9600); //Conecta al COM3 con un serial rate de 9600
+            arduino.arduinoTX("COM3", 1000000); //Conecta al COM3 con un serial rate de 1000000
         } catch (ArduinoException ex) {
             Logger.getLogger(CSGOLeds.class.getName()).log(Level.SEVERE, null, ex);
             System.exit(1);
         }
     }
     
-    public static void main(String[] args) throws ArduinoException, SerialPortException {
+    public static void main(String[] args) {
         CSGOLeds main = new CSGOLeds();
         //while(main.ejecutando){
-            //TODO Comprobaciones del juego
+        //TODO Comprobaciones del juego
         //}
     }
 }
