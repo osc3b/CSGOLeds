c6cb379272b6c8ea43d563be3d35b12b1ee5deeb
osc3b <osc3b@Osc3b-Portatil>
2019-05-11 18:23:39 +0200

    Nuevos eventos en Java
    - Nuevo sistema kills y flash
    - Bomba plantada: 5
    - Bomba explota: 6
    - Bomba desmantelada: 7
    - Porcentaje balas cargador: 300-400

--- src/main/java/me/osc3b/csgoleds/CSGOVariables.java
+++ src/main/java/me/osc3b/csgoleds/CSGOVariables.java
@@ -10,12 +10,15 @@ package me.osc3b.csgoleds;
  * @author Oscar
  */
 
+import com.brekcel.csgostate.JSON.Weapon;
 import com.brekcel.csgostate.post.PostHandlerAdapter;
+import static java.lang.Math.round;
 
 public class CSGOVariables extends PostHandlerAdapter {
     
     int killsguardadas = 0;
     String teamAct = "NULL";
+    int porcentajeCargador = 0;
     
     @Override
     public void playerHealthChange(int health) { 
@@ -24,7 +27,7 @@ public class CSGOVariables extends PostHandlerAdapter {
     }
 
     @Override
-    public void playerMatchKillsChange(int kills) {
+    public void playerRoundKillsChange(int kills) {
         System.out.println("***** Nueva kill. Kills: " + kills + " *****");
         if(kills > killsguardadas){
             CSGOLeds.arduino.serialWrite('1');
@@ -34,13 +37,15 @@ public class CSGOVariables extends PostHandlerAdapter {
     
     @Override
     public void playerFlashedChange(int flashed){
-        System.out.println("***** FLASHED ******");
-        CSGOLeds.arduino.serialWrite('2');
+        System.out.println("***** FLASHED " + flashed + "******");
+        if(flashed > 180) //De 0 a 255
+            CSGOLeds.arduino.serialWrite('2');
     }
     
     @Override
-    public void roundChange(int round){
+    public void roundReset(){
         System.out.println("======================================\n Nueva ronda \n==============================================");
+        killsguardadas = 0;
     }
     
     @Override
@@ -58,4 +63,46 @@ public class CSGOVariables extends PostHandlerAdapter {
             CSGOLeds.arduino.serialWrite('4');
         }
     }
+    
+    @Override
+    public void bombPlanted(){
+        System.out.println("BOMBA PLANTADA");
+        CSGOLeds.arduino.serialWrite('5');
+    }
+    
+    @Override
+    public void bombExploded(){
+        System.out.println("LA BOMBA HA EXPLOTADO");
+        CSGOLeds.arduino.serialWrite('6');
+    }
+    
+    @Override
+    public void bombDefused(){
+        System.out.println("BOMBA DESMONTADA");
+        CSGOLeds.arduino.serialWrite('7');
+    }
+    
+    @Override
+    public void weaponActiveChange(Weapon weapon){
+        System.out.println("Arma cambiada a: " + weapon.getName());
+        System.out.println("(C) Balas en el cargador: " + weapon.getAmmoClip() + "/" + weapon.getAmmoClipMax());
+        porcentajeCargador = round((float)(weapon.getAmmoClip()*100)/weapon.getAmmoClipMax());
+        //Envia primero 0% balas durante 100ms ya que cambia de arma, luego el % cargador
+        CSGOLeds.arduino.serialWrite(Integer.toString(300)+Integer.toString(porcentajeCargador+300), 3, 100);
+    }
+    
+    @Override
+    public void weaponShoot(Weapon weapon){
+        System.out.println("(S) Balas en el cargador: " + weapon.getAmmoClip() + "/" + weapon.getAmmoClipMax());
+        porcentajeCargador = round((float)(weapon.getAmmoClip()*100)/weapon.getAmmoClipMax());
+        CSGOLeds.arduino.serialWrite(Integer.toString(porcentajeCargador+300));
+    }
+    
+    @Override
+    public void weaponReload(Weapon weapon){
+        System.out.println("(R) Balas en el cargador: " + weapon.getAmmoClip() + "/" + weapon.getAmmoClipMax());
+        porcentajeCargador = 100;
+        CSGOLeds.arduino.serialWrite(Integer.toString(400));
+    }
 }
+
